6cab3da4f2032e0d09d89f07f06660afb462b98e
Kathan Cheema <19304291+cheemcheem@users.noreply.github.com>
2025-12-10 00:25:18 +0000

    Day 9 Part 1

--- challenge/days/day9.ts
+++ challenge/days/day9.ts
@@ -1,8 +1,29 @@
-import { Day } from '@cheemcheem/adventofcode-common';
+import { createGrid, Day, getInput, getInputSplitByLine, inputAsGrid, positionEquals } from '@cheemcheem/adventofcode-common';
 
 const day: Day = {
   part1: async (dayNumber, example) => {
-    return 0;
+    const coordStrings = await getInputSplitByLine(dayNumber, example);
+    const coords = coordStrings.map((coordString) => {
+      const [x, y] = coordString.split(',').map(Number);
+      return { x, y };
+    });
+    // const gridSize = coords.reduce((a, b) => ({
+    //   x: a.x > b.x ? a.x : b.x,
+    //   y: a.y > b.y ? a.y : b.y,
+    // }));
+    // const grid = createGrid();
+    const maxAreas = coords.map((a, index, array) => {
+      if (index === array.length - 1) {
+        return 0;
+      }
+      const areas = array.slice(index + 1).map(b => (Math.abs(a.x - b.x) + 1) * (Math.abs(a.y - b.y) + 1)).sort((a, b) => b - a);
+      // console.log({ areas });
+
+      return areas[0];
+    }).sort((a, b) => b - a);
+    // console.log({ maxAreas });
+
+    return maxAreas[0];
   },
   part2: async (dayNumber, example) => {
     return 0;
